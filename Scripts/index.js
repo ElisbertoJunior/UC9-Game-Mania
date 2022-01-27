91bfe44681438001bf72add20d4e05fad2a309df
Elisberto Junior <elisberto_jr@hotmail.com>
2022-01-27 16:29:27 -0300

    first commit

---
+++ Scripts/index.js
@@ -0,0 +1,13 @@
+let icon = document.querySelector('#icon');
+
+icon.addEventListener('click', ()=>{ 
+
+    let nav = document.querySelector('#nav'); 
+
+    if(nav.style.display != "flex"){
+        return nav.style.display = "flex";
+    } else {
+        return nav.style.display = "none";
+    }
+    
+});
